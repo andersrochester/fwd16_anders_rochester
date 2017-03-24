ca46b087ae5960a903c3bbb7426f1b713f2902e2
Anders Rochester <anders.rochester@gmail.com>
2017-03-24 12:07:55 +0100

    Delete Query2.sql

--- Query2.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    a.description, COUNT(fa.depID) AS dep_count
-FROM
-    manifest.crew AS fa
-        JOIN
-    manifest.departement AS a ON a.depID = fa.depID
-GROUP BY fa.depID
-ORDER BY dep_count , a.description;
